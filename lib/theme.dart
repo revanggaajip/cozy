568c84eb8786d90fce172564c7223629fe4829e1
Revangga Aji Pratama <revanggaajip2@gmail.com>
2022-09-29 13:16:49 +0700

    Add Color Theme

---
+++ lib/theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+Color purpleColor = const Color(0xff5843BE);
+Color orangeColor = const Color(0xffFF9376);
+Color blackColor = const Color(0xff000000);
+Color whiteColor = const Color(0xffffffff);
+Color greyColor = const Color(0xff82868E);
+
+
