c349a2596da0fa4e253a163f702999e89d8f2301
Revangga Aji Pratama <revanggaajip2@gmail.com>
2022-10-03 17:28:17 +0700

    Create Bottom Navigation Widgets

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:cozy/models/space.dart';
 import 'package:cozy/models/tips.dart';
 import 'package:cozy/theme.dart';
+import 'package:cozy/widgets/bottom_navbar_item.dart';
 import 'package:cozy/widgets/city_card.dart';
 import 'package:cozy/widgets/space_card.dart';
 import 'package:cozy/widgets/tips_card.dart';
@@ -12,183 +13,219 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.symmetric(vertical: edge),
-          child: ListView(
-            children: [
-              // NOTE : Title / Header
-              Padding(
-                padding: EdgeInsets.only(left: edge),
-                child: Text(
-                  'Explore Now',
-                  style: blackTextStyle.copyWith(
-                    fontSize: 24,
+        child: ListView(
+          children: [
+            SizedBox(
+              height: edge,
+            ),
+            // NOTE : Title / Header
+            Padding(
+              padding: EdgeInsets.only(left: edge),
+              child: Text(
+                'Explore Now',
+                style: blackTextStyle.copyWith(
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 2,
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: edge),
+              child: Text(
+                'Mencari kosan yang cozy',
+                style: greyTextStyle.copyWith(fontSize: 16),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            // NOTE : Popular Cities
+            Padding(
+              padding: EdgeInsets.only(left: edge),
+              child: Text(
+                'Popular Cities',
+                style: regularTextStyle.copyWith(fontSize: 16),
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Container(
+              height: 150,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  SizedBox(width: 24),
+                  CityCard(
+                    City(
+                        id: 1,
+                        name: 'Jakarta',
+                        imageUrl: 'assets/images/city1.png'),
                   ),
-                ),
-              ),
-              SizedBox(
-                height: 2,
-              ),
-              Padding(
-                padding: EdgeInsets.only(left: edge),
-                child: Text(
-                  'Mencari kosan yang cozy',
-                  style: greyTextStyle.copyWith(fontSize: 16),
-                ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              // NOTE : Popular Cities
-              Padding(
-                padding: EdgeInsets.only(left: edge),
-                child: Text(
-                  'Popular Cities',
-                  style: regularTextStyle.copyWith(fontSize: 16),
-                ),
-              ),
-              SizedBox(
-                height: 16,
-              ),
-              Container(
-                height: 150,
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: [
-                    SizedBox(width: 24),
-                    CityCard(
-                      City(
-                          id: 1,
-                          name: 'Jakarta',
-                          imageUrl: 'assets/images/city1.png'),
-                    ),
-                    SizedBox(width: 20),
-                    CityCard(
-                      City(
-                          id: 2,
-                          name: 'Bandung',
-                          imageUrl: 'assets/images/city2.png',
-                          isPopular: true),
-                    ),
-                    SizedBox(
-                      width: 20,
-                    ),
-                    CityCard(
-                      City(
-                          id: 3,
-                          name: 'Surabaya',
-                          imageUrl: 'assets/images/city3.png'),
-                    ),
-                    SizedBox(
-                      width: 24,
-                    )
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              // NOTE : Recomended Spaces.
-              Padding(
-                padding: EdgeInsets.only(left: edge),
-                child: Text(
-                  'Recommended Space',
-                  style: regularTextStyle.copyWith(fontSize: 16),
-                ),
-              ),
-              SizedBox(
-                height: 16,
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: edge),
-                child: Column(
-                  children: [
-                    SpaceCard(
-                      Space(
-                        id: 1,
-                        name: 'Alun-alun',
-                        imageUrl: 'assets/images/space1.png',
-                        price: 52,
-                        city: 'Pekalongan',
-                        country: 'Indonesia',
-                        rating: 5,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    SpaceCard(
-                      Space(
+                  SizedBox(width: 20),
+                  CityCard(
+                    City(
                         id: 2,
-                        name: 'Gedung Garuda',
-                        imageUrl: 'assets/images/space2.png',
-                        price: 11,
-                        city: 'Batang',
-                        country: 'Indonesia',
-                        rating: 4,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    SpaceCard(
-                      Space(
+                        name: 'Bandung',
+                        imageUrl: 'assets/images/city2.png',
+                        isPopular: true),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  CityCard(
+                    City(
                         id: 3,
-                        name: 'Konoha Village',
-                        imageUrl: 'assets/images/space3.png',
-                        price: 7,
-                        city: 'Konoha',
-                        country: 'Indonesia',
-                        rating: 3,
-                      ),
-                    ),
-                  ],
-                ),
+                        name: 'Surabaya',
+                        imageUrl: 'assets/images/city3.png'),
+                  ),
+                  SizedBox(
+                    width: 24,
+                  )
+                ],
               ),
-              SizedBox(
-                height: 30,
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            // NOTE : Recomended Spaces.
+            Padding(
+              padding: EdgeInsets.only(left: edge),
+              child: Text(
+                'Recommended Space',
+                style: regularTextStyle.copyWith(fontSize: 16),
               ),
-              // NOTE : Tips & guidance
-              Padding(
-                padding: EdgeInsets.only(left: edge),
-                child: Text(
-                  'Tips & Guidance',
-                  style: regularTextStyle.copyWith(fontSize: 16),
-                ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: edge),
+              child: Column(
+                children: [
+                  SpaceCard(
+                    Space(
+                      id: 1,
+                      name: 'Alun-alun',
+                      imageUrl: 'assets/images/space1.png',
+                      price: 52,
+                      city: 'Pekalongan',
+                      country: 'Indonesia',
+                      rating: 5,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  SpaceCard(
+                    Space(
+                      id: 2,
+                      name: 'Gedung Garuda',
+                      imageUrl: 'assets/images/space2.png',
+                      price: 11,
+                      city: 'Batang',
+                      country: 'Indonesia',
+                      rating: 4,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  SpaceCard(
+                    Space(
+                      id: 3,
+                      name: 'Konoha Village',
+                      imageUrl: 'assets/images/space3.png',
+                      price: 7,
+                      city: 'Konoha',
+                      country: 'Indonesia',
+                      rating: 3,
+                    ),
+                  ),
+                ],
               ),
-              SizedBox(
-                height: 16,
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            // NOTE : Tips & guidance
+            Padding(
+              padding: EdgeInsets.only(left: edge),
+              child: Text(
+                'Tips & Guidance',
+                style: regularTextStyle.copyWith(fontSize: 16),
               ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: edge),
-                child: Column(
-                  children: [
-                    TipsCard(
-                      Tips(
-                        id: 1,
-                        title: 'City Guidelines',
-                        imageUrl: 'assets/images/tips1.png',
-                        updatedAt: '20 Apr',
-                      ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: edge),
+              child: Column(
+                children: [
+                  TipsCard(
+                    Tips(
+                      id: 1,
+                      title: 'City Guidelines',
+                      imageUrl: 'assets/images/tips1.png',
+                      updatedAt: '20 Apr',
                     ),
-                    SizedBox(
-                      height: 20,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TipsCard(
+                    Tips(
+                      id: 2,
+                      title: 'Pekalongan Fairship',
+                      imageUrl: 'assets/images/tips2.png',
+                      updatedAt: '11 Dec',
                     ),
-                    TipsCard(
-                      Tips(
-                        id: 2,
-                        title: 'Pekalongan Fairship',
-                        imageUrl: 'assets/images/tips2.png',
-                        updatedAt: '11 Dec',
-                      ),
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+          ],
         ),
       ),
+      floatingActionButton: Container(
+        height: 65,
+        width: MediaQuery.of(context).size.width - (2 * edge),
+        margin: EdgeInsets.symmetric(
+          horizontal: edge,
+        ),
+        decoration: BoxDecoration(
+          color: Color(0xffF6F7F8),
+          borderRadius: BorderRadius.circular(23),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            BottomNavbarItem(
+              imageUrl: 'assets/images/icon_home.png',
+              isActive: true,
+            ),
+            BottomNavbarItem(
+              imageUrl: 'assets/images/icon_mail.png',
+              isActive: false,
+            ),
+            BottomNavbarItem(
+              imageUrl: 'assets/images/icon_card.png',
+              isActive: false,
+            ),
+            BottomNavbarItem(
+              imageUrl: 'assets/images/icon_love.png',
+              isActive: false,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
